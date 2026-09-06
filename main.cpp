3f2616cce370f3703d822262b39dea5385c650e6
IamAKS26 <vu1f2324045@pvppcoe.ac.in>
2026-09-06 19:19:06 +0530

    Add intentional bugs for testing

--- main.cpp
+++ main.cpp
@@ -16,4 +16,22 @@ int main() {
     cout << "Result: " << divide(10, 0) << endl;
 
     return 0;
+}#include <iostream>
+using namespace std;
+
+int divide(int a, int b) {
+    return a / b;
+}
+
+int main() {
+
+    int numbers[] = {10, 20, 30};
+
+    for (int i = 0; i <= 3; i++) {
+        cout << numbers[i] << endl;
+    }
+
+    cout << "Result: " << divide(10, 0) << endl;
+
+    return 0;
 }
